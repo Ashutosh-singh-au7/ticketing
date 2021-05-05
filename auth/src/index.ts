15f0e4a18dbebca50e8cac52456a554ecd3a683a
Ashutosh-singh-au7 <ashutosh805280@gmail.com>
2021-05-05 12:52:00 +0530

    updated startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async ()=>{
-    console.log('Starting up Auth....')
+    console.log('Starting up Auth...........')
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined');
     }
