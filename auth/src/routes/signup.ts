8d2a902b01ed8a849f3416b31b03fe1805825597
Ashutosh-singh-au7 <ashutosh805280@gmail.com>
2021-05-05 13:21:54 +0530

    Mailgun checking

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -7,8 +7,8 @@ import { validateRequest, BadRequestError } from '@reapertickets/common';
 
 const router = express.Router();
 
-const DOMAIN = 'sandboxd836eba6712a4627830d33aaef4e07ec.mailgun.org';
-const mg = mailgun({apiKey: 'cedfc18de9adc23b7a8c98ec2250f5d4-71b35d7e-7dd2c8fe' , domain: DOMAIN})
+// const DOMAIN = 'sandboxd836eba6712a4627830d33aaef4e07ec.mailgun.org';
+// const mg = mailgun({apiKey: 'cedfc18de9adc23b7a8c98ec2250f5d4-71b35d7e-7dd2c8fe' , domain: DOMAIN})
 
 router.post('/api/users/signup',
 [
